9299f966bbda90dc74bad6a9cd56afd399b03dd4
go467913 <go467913@ajou.ac.kr>
2018-02-22 17:14:51 +0900

    단지수 주석 추가

--- 2667_danji/2667_danji/main.cpp
+++ 2667_danji/2667_danji/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int visited[25][25] = { 0 };
+int visited[25][25] = { 0 };		//방문 했던 기록을 남기는 배열 (단지수로 체크)
 char map[25][25] = { 0 };
 int map_size = 0;
 int danji = 0;
@@ -63,34 +63,34 @@ int main()
 	{
 		for (int c = 0; c < map_size; c++)
 		{
-			if (map[r][c] == '1' && visited[r][c] == 0)
+			if (map[r][c] == '1' && visited[r][c] == 0)		//아파트가 있고, 방문을 안했던 곳이라면
 			{
-				visited[r][c] = cnt;
-				find_danji(r, c);
-				cnt++;
+				visited[r][c] = cnt;			//현재 단지수를 표기
+				find_danji(r, c);				//근처에 이어진 곳이 있는지 찾아보기
+				cnt++;							//다 찾았으면 단지 번호 증가
 			}
 		}
 	}
 	
-	cout << cnt - 1 << endl;
+	cout << cnt - 1 << endl;		//단지 수를 출력
 
-	int *ans = new int[cnt-1];
+	int *ans = new int[cnt-1];		//단지 수 만큼 배열 생성
 
 	for (int i = 1; i < cnt; i++)
 	{
 		int num_danji = 0;
 		for (int r = 0; r < map_size; r++)
 		{
-			for (int c = 0; c < map_size; c++)
+			for (int c = 0; c < map_size; c++)		//각 단지가 몇 동씩 갖고 있는지 확인
 			{
 				if (visited[r][c] == i)
 					num_danji++;
 			}
 		}
-		ans[i-1] = num_danji;
+		ans[i-1] = num_danji;			//각 단지별 배열에 저장
 	}
 
-	for (int i = 0; i < cnt - 1; i++)
+	for (int i = 0; i < cnt - 1; i++)		//오름차순으로 정렬
 	{
 		for (int j = i + 1; j < cnt - 1; j++)
 		{
